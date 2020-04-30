d047c2a4b9d56e894ad2e9493975e9a5502c90c9
Rafa Tarin <rafa@MacBook-Pro-de-Rafa.local>
2020-04-30 10:57:08 +0200

    "api key actualizada"

--- public/src/scriptDatos.js
+++ public/src/scriptDatos.js
@@ -1,6 +1,6 @@
 async function datos() {
 	
-	let url = ("https://api.penguinrandomhouse.com/resources/works?search=" + document.getElementById("id_busqueda").value + "&start=0&max=15&expandLevel=1&key=9kjnk53qkh6qy6vrca7fsug4");
+	let url = ("https://api.penguinrandomhouse.com/resources/works?search=" + document.getElementById("id_busqueda").value + "&start=0&max=15&expandLevel=1&key=7edmxe4esacyxr4w4t4ympv2");
 	let datos = await fetch(url);
 	let Json = await datos.json();
 	console.log(Json);
